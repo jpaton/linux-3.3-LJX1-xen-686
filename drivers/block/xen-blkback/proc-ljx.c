82df07c79eab519baa97c31b012cd225e42b1d3c
James Paton <paton@cs.wisc.edu>
2012-05-25 12:51:58 -0500

    finished seq_show and ljx_printf Signed-off-by: James Paton <paton@cs.wisc.edu>

--- drivers/block/xen-blkback/proc-ljx.c
+++ drivers/block/xen-blkback/proc-ljx.c
@@ -9,9 +9,11 @@
 
 #define DATA_SIZE 1024
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 struct proc_dir_entry *proc_file;
-static loff_t offset = 0; 
 static LIST_HEAD(buffer_list);
+static struct buffer *current_buffer;
 
 struct buffer {
 	struct list_head list;
@@ -19,23 +21,56 @@ struct buffer {
 	loff_t len;
 };
 
+static struct buffer *init_buf(void) {
+	struct buffer *ret = kmalloc(sizeof(char) * DATA_SIZE, 0);
+	ret->len	= 0,
+	ret->buf	= (char *)kmalloc(sizeof(char) * DATA_SIZE, 0),
+	memset(ret->buf, 0, sizeof(char) * DATA_SIZE);
+
+	return ret;
+}
+
 struct user_pos {
 	struct buffer *buf;
 	loff_t offset;
 };
 
 int ljx_print(const char *fmt, ...) {
-	/*va_list args;
+	va_list args;
 	int written;
-	char buf[DATA_SIZE];
-	char * tail;
+	int lefttocopy;
+	int tocopy;
+	int bufsize = 0;
+	int spaceleft;
+	char *buf = NULL;
 
-	va_start(args, fmt);
-	str_size = scnprintf(buf, DATA_SIZE, fmt, args);
-	va_end(args);
+	do {
+		/* keep allocating a larger buffer until we get one large enough */
+		bufsize += sizeof(char) * 1024;
+		kfree(buf);
+		buf = kmalloc(bufsize, 0);
+		va_start(args, fmt);
+		written = vsnprintf(buf, bufsize, fmt, args);
+		va_end(args);
+	} while (written > bufsize);
 
-	return size;
-	*/
+	/* TODO: protect access to buffer_list with lock */
+	/* copy contents of buf into buffer list, one buffer at a time */
+	lefttocopy = bufsize;
+	while (lefttocopy > 0) {
+		spaceleft = DATA_SIZE - current_buffer->len;
+		tocopy = MAX(spaceleft, lefttocopy);
+		memcpy(&current_buffer->buf[current_buffer->len], buf, tocopy);
+		buf += tocopy;
+		lefttocopy -= tocopy;
+		if (lefttocopy > 0) {
+			/* need to allocate a new buffer */
+			current_buffer = init_buf();
+			list_add_tail(&current_buffer->list, &buffer_list);
+		}
+	}
+
+	kfree(buf);
 	return 0;
 }
 
@@ -72,34 +107,36 @@ static void *ljx_seq_start(struct seq_file *s, loff_t *pos) {
 
 static void *ljx_seq_next(struct seq_file *s, void *v, loff_t *pos) {
 	struct user_pos *upos;
-	size_t buf_num;
+	loff_t byte_in_buf;
+       
+	upos = (struct user_pos *) v;
+	byte_in_buf = ++upos->offset % DATA_SIZE;
+
+	if (byte_in_buf == 0) {
+		if (upos->buf->list.next == &buffer_list)
+			/* we have reached the end */
+			return 0;
+		/* must move to next buf */
+		upos->buf = list_entry(upos->buf->list.next, struct buffer, list);
+	}
+	if (byte_in_buf >= upos->buf->len) {
+		/* we have reached the very end! */
+		return 0;
+	}
+
+	return upos;
+}
+
+static int ljx_seq_show(struct seq_file *s, void *v) {
+	struct user_pos *upos;
 	loff_t byte_in_buf;
        
 	upos = (struct user_pos *) v;
 	byte_in_buf = upos->offset % DATA_SIZE;
 
-	if (byte_in_buf + 1 >= DATA_SIZE) {
-		/* must move to next buf */
-		upos->buf = list_entry(upos->buf->list.next, struct buffer, list);
-	}
+	seq_printf(s, "%c", upos->buf->buf[byte_in_buf]);
 
-	upos->offset++;
-	return upos;
-}
-
-static int ljx_seq_show(struct seq_file *s, void *v) {
-	// TODO
-	//loff_t *spos = (loff_t *) v;
 	return 0;
-}
-
-static struct buffer *init_buf(void) {
-	struct buffer *ret = kmalloc(sizeof(char) * DATA_SIZE, 0);
-	ret->len	= 0,
-	ret->buf	= (char *)kmalloc(sizeof(char) * DATA_SIZE, 0),
-	memset(ret->buf, 0, sizeof(char) * DATA_SIZE);
-
-	return ret;
 }
 
 void ljx_init() {
@@ -118,6 +155,7 @@ void ljx_init() {
 	/* initialize buffer list */
 	struct buffer *first_buf = init_buf();
 	list_add_tail(&first_buf->list, &buffer_list);
+	current_buffer = first_buf;
 }
 
 int procfile_read(char *buffer,
